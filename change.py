95db6a944599b901b44c159dbe6e7d62939f2dc7
sadegh khajepour <sakha1370@gmail.com>
2025-08-24 14:31:14 +0330

    fix bug

--- change.py
+++ change.py
@@ -1,27 +1,15 @@
-import subprocess
-import base64
+from pyclash import Clash
 
 def main():
 
+    with open("test.txt") as f:
+        nodes = [line.strip() for line in f if line.strip()]
 
-    file_path = "test.txt"
+    clash = Clash()
+    for node in nodes:
+        clash.add_node_from_url(node)
 
-    with open(file_path, "r") as f:
-        lines = f.readlines()
-
-    for i, line in enumerate(lines, start=1):
-        node = line.strip()
-        if not node:
-            continue
-
-        # Encode node in base64 for SubConverter API
-        node_b64 = base64.b64encode(node.encode()).decode()
-
-        url = f"http://127.0.0.1:25500/sub?target=clash&list=true&url=data:text/plain;base64,{node_b64}"
-        output_file = f"clash_{i}.yaml"
-
-        subprocess.run(["curl", url, "-o", output_file], check=True)
-        print(f"{output_file} created successfully!")
+    clash.write_yaml("clash.yaml")
 
 
 if __name__ == "__main__":
